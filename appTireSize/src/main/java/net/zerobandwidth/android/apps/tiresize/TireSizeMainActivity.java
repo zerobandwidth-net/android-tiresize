8d605356484f6e80a892901d78d06e2e48156cae
zerobandwidth <pasha.paterson@gmail.com>
2018-02-13 17:11:31 -0500

    (#1) Initial commit of blank Android project.

---
+++ appTireSize/src/main/java/net/zerobandwidth/android/apps/tiresize/TireSizeMainActivity.java
@@ -0,0 +1,15 @@
+package net.zerobandwidth.android.apps.tiresize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TireSizeMainActivity
+extends AppCompatActivity
+{
+	@Override
+	protected void onCreate( Bundle bndlState )
+	{
+		super.onCreate(bndlState) ;
+		setContentView( R.layout.act_tiresize_main ) ;
+	}
+}
